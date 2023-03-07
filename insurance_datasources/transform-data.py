a7490c94478b1831bd0c94055cfcedc3c4f37113
FarzeenAzhar-oc <119649459+FarzeenAzhar-oc@users.noreply.github.com>
2023-03-07 15:02:11 +0530

    Update transform-data.py
    Added documentation

--- insurance_datasources/transform-data.py
+++ insurance_datasources/transform-data.py
@@ -5,6 +5,21 @@ import pandas as pd
 from sklearn import preprocessing
 
 def getColNames(dataframe, col):
+    """
+    Function to  do a case insensitve search for column names in dataframe 
+    corresponding to the ones given in col and return the output
+    
+    Parameters
+    ----------
+    dataframe: dataframe,
+        Dataframe frome which the column names must be take
+    col: str, list
+        Column names to search for in the dataframe
+        
+    Returns
+    -------
+        A list/string of column names from dataframe corresponding to the one(s) given in col
+    """
     if type(col) == list:
         col = list(map(lambda x: x.lower(), col))
         return [x for x in df.columns if x.lower() in col]
